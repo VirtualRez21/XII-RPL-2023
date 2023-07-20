68ebe01310d7e4d47d4b7c56270bc6e3690a8586
UyizDofukizi <57898357+VirtualRez21@users.noreply.github.com>
2023-07-20 15:27:40 +0800

    Add files via upload

---
+++ Modul-Teori/index.php
@@ -0,0 +1,25 @@
+<?php
+
+	echo "Hellow World!";
+
+	$kata = "SMK Ketopong";
+	$tahun = 2023;
+
+	echo "$kata tahun $tahun";
+	echo var_dump($tahun);
+
+	$a = 10;
+	$b = 22;
+	$c = $a + $b; // penambahan
+	$d = $b - $a; // pengurangan
+	$e = $a * $b; // perkalian
+	$f = $b / $a; // pembagian
+	$g = $b % $a; // modulus
+
+	echo "<br>".$c."<br>";
+	echo $d."<br>";
+	echo $e."<br>";
+	echo $f."<br>";
+	echo $g."<br>";
+
+?>
